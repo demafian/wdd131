cece320b69fa02ee52eaf275398e2f621e0f5c0b
Your Name <your-email@example.com>
2026-02-03 15:34:37 +0200

    Finalized CSS and passed all audit requirements

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -6,7 +6,7 @@ const temples = [
     { templeName: "Washington D.C.", location: "Kensington, Maryland, United States", dedicated: "1974, November, 19", area: 156558, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg" },
     { templeName: "Lima Perú", location: "Lima, Perú", dedicated: "1986, January, 10", area: 9600, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg" },
     { templeName: "Mexico City Mexico", location: "Mexico City, Mexico", dedicated: "1983, December, 2", area: 116642, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg" },
-    // 3 Unique Added Temples (Replacement for Barranquilla/Salt Lake)
+    // 3 Unique Added Temples 
     { templeName: "Logan Utah", location: "Logan, Utah, United States", dedicated: "1884, May, 17", area: 119619, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/logan-utah/400x250/logan-temple-768119-wallpaper.jpg" },
     { templeName: "St. George Utah", location: "St. George, Utah, United States", dedicated: "1877, April, 6", area: 110000, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/st-george-utah/400x250/st-george-temple-lds-149536-wallpaper.jpg" },
     { templeName: "Paris France", location: "Le Chesnay, France", dedicated: "2017, May, 21", area: 44175, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/paris-france/400x250/paris-france-temple-exterior-1905503.jpg" },
